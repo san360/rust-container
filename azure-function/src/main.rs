02578079f0ef08be018b5381d4f1d68db2c94728
Sanjay Singh <san360@users.noreply.github.com>
2023-09-04 15:11:30 +0000

    added files

--- azure-function/src/main.rs
+++ azure-function/src/main.rs
@@ -1,28 +1,18 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+// src/main.rs
 
-// Import necessary dependencies from Actix Web framework
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    // Define a handler function for GET requests to the root path ("/")
-    // The function returns a type that implements the Responder trait
-
-    HttpResponse::Ok().body("Hello, World!")
-    // Return an HTTP response with a status code of 200 (OK)
-    // and a response body of "Hello, World!"
+    HttpResponse::Ok().body("Hello, Azure Function!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Entry point of the application
-
     HttpServer::new(|| {
-        // Create a new instance of the HttpServer
-
         App::new().service(hello)
-        // Create a new instance of the App and register the hello function 
-
-    }).bind("127.0.0.1:8080")?.run().await
-    // Bind the server to the IP address and port
-    // Start the server and await its completion
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
